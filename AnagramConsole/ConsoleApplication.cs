773aeb01a65404451eeb734c66e0bc998a663ffc
Daniel <webezine@hotmail.co.uk>
2021-06-21 23:09:00 +0100

    Created console application model

---
+++ AnagramConsole/ConsoleApplication.cs
@@ -0,0 +1,19 @@
+﻿using AnagramConsole.Services;
+
+namespace AnagramConsole
+{
+    public class ConsoleApplication
+    {
+        private readonly IAnagramChecker _anagramChecker;
+
+        public ConsoleApplication(IAnagramChecker anagramChecker)
+        {
+            _anagramChecker = anagramChecker;
+        }
+
+        public void Run()
+        {
+            _anagramChecker.FetchAndOutputAnagramResults();
+        }
+    }
+}
