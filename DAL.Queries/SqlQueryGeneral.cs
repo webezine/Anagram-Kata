f326346a22746d024924f5b4d85294ed03f863d1
Daniel <webezine@hotmail.co.uk>
2021-06-21 23:05:52 +0100

    Added const class for sprc naming

---
+++ DAL.Queries/SqlQueryGeneral.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL.Queries
+{
+    public static class SqlQueryGeneral
+    {
+        // Anagram Repository
+        public const string GetAnagram = "dbo.AC_Get_Anagrams";
+        public const string GetAnagramByWord = "dbo.AC_Get_Angaram_ByWord";
+        public const string GetAnagramById = "dbo.AC_Get_Angaram_ById";
+    }
+}
